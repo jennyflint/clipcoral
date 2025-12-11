84d191c417677f2b71d081d883e8ec5ff8e4fd94
homone112@gmail.com <homone112@gmail.com>
2025-12-11 23:06:52 +0200

    add grumphp and php cs fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'declare_strict_types' => true,
+        'yoda_style' => false,
+    ])
+    ->setRiskyAllowed(true)
+    ->setFinder($finder)
+;
